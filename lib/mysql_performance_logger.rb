fac66b541deca9b518fd592b630ddb80114e3e6e
Masashi Fujita <masashifujita536@gmail.com>
2023-10-10 01:06:32 +0900

    initial setup

---
+++ lib/mysql_performance_logger.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mysql_performance_logger/version"
+
+module MysqlPerformanceLogger
+  class Error < StandardError; end
+  # Your code goes here...
+end
